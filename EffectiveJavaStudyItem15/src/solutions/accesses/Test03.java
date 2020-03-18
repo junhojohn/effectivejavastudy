db4445074ef1bc0409be6ee185c53fdf4a57eccd
junhojohn <junhojohn@gmail.com>
2020-03-18 13:24:05 +0900

    private static final 으로 선언된 배열에 접근 하는 방법2

---
+++ EffectiveJavaStudyItem15/src/solutions/accesses/Test03.java
@@ -0,0 +1,9 @@
+package solutions.accesses;
+
+public class Test03 {
+	private static final String[] sampleArr = {"Hello"};
+
+	public static final String[] values(){
+		return sampleArr.clone();
+	}
+}
