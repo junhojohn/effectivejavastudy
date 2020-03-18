0c767951db9fc23cedce155fdf27e285491ac9e4
junhojohn <junhojohn@gmail.com>
2020-03-18 13:22:24 +0900

    배열은 public static final 선언하지 말자..

--- EffectiveJavaStudyItem15/src/solutions/accesses/Test02.java
+++ EffectiveJavaStudyItem15/src/solutions/accesses/Test02.java
@@ -1,5 +1,11 @@
 package solutions.accesses;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Test02 {
 	private static final String[] sampleArr = {"Hello"};
+	
+	public static final List<String> ARR = Collections.unmodifiableList(Arrays.asList(sampleArr));
 }
